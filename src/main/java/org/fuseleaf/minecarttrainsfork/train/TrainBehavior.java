1e172037d1650a5682c57a8b643a4b3c6784f111
KSSJW <165921128+KSSJW@users.noreply.github.com>
2026-09-14 13:07:58 +0800

    refactor: refactor TrainBehavior.java

--- src/main/java/org/fuseleaf/minecarttrainsfork/train/TrainBehavior.java
+++ src/main/java/org/fuseleaf/minecarttrainsfork/train/TrainBehavior.java
@@ -5,7 +5,6 @@ import org.fuseleaf.minecarttrainsfork.config.ConfigManager;
 import org.fuseleaf.minecarttrainsfork.network.NetworkManager;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.vehicle.minecart.AbstractMinecart;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -13,84 +12,74 @@ import net.minecraft.world.phys.Vec3;
 
 public class TrainBehavior {
 
-    public static void tick(AbstractMinecart entity) {
-        if (!entity.level().isClientSide()) {
+    public static void tick(AbstractMinecart cart) {
+        if (cart == null || cart.level().isClientSide()) {
+            return;
+        }
 
-            Chainable entityIChainable = (Chainable)entity;
+        Chainable cartChainable = (Chainable)cart;
+        AbstractMinecart parentCart = cartChainable.getChainedParent();
+        AbstractMinecart childCart = cartChainable.getChainedChild();
 
-            if (entityIChainable.getChainedParent() != null) {
-                double distance = entityIChainable.getChainedParent().distanceTo(entity) - 1;
+        if (parentCart != null) {
+            double distance = parentCart.distanceTo(cart) - 1;
 
-                if (distance <= 4) {
-                    Vec3 directionToParent = entityIChainable.getChainedParent().position().subtract(entity.position()).normalize();
+            if (distance <= 4) {
+                Vec3 directionToParent = parentCart.position().subtract(cart.position()).normalize();
 
-                    double cartSpacing = ConfigManager.getCartSpacing();
+                double cartSpacing = ConfigManager.getCartSpacing();
 
-                    if (distance > cartSpacing) {
-                        Vec3 parentVelocity = entityIChainable.getChainedParent().getDeltaMovement();
+                if (distance > cartSpacing) {
+                    Vec3 parentVelocity = parentCart.getDeltaMovement();
 
-                        if (parentVelocity.length() < 0.1) {
-                            entity.setDeltaMovement(directionToParent.scale(0.05));
-                        } else {
-                            entity.setDeltaMovement(directionToParent.scale(parentVelocity.length()));
-                            entity.setDeltaMovement(entity.getDeltaMovement().scale(distance));
-                        }
-                    } else if (distance < cartSpacing - 0.2) {
-                        entity.setDeltaMovement(directionToParent.scale(-0.05));
+                    if (parentVelocity.length() < 0.1) {
+                        cart.setDeltaMovement(directionToParent.scale(0.05));
                     } else {
-                        entity.setDeltaMovement(Vec3.ZERO);
+                        cart.setDeltaMovement(directionToParent.scale(parentVelocity.length()));
+                        cart.setDeltaMovement(cart.getDeltaMovement().scale(distance));
                     }
+                } else if (distance < cartSpacing - 0.2) {
+                    cart.setDeltaMovement(directionToParent.scale(-0.05));
                 } else {
-                    AbstractMinecart parentCart = entityIChainable.getChainedParent();
+                    cart.setDeltaMovement(Vec3.ZERO);
+                }
+            } else {
+                if (ConfigManager.isEnabledBrakingAfterTrainSeparation()) {
+                    AbstractMinecart currentCart = cart;
+                    Chainable currentCartChainable = (Chainable) currentCart;
+                    AbstractMinecart currentParentCart = currentCartChainable.getChainedParent();
 
-                    if (ConfigManager.isEnabledBrakingAfterTrainSeparation()) {
-                        for (AbstractMinecart cart = entity; ((Chainable)cart).getChainedParent() != null; cart = (AbstractMinecart)((Chainable)cart).getChainedParent()) {
-                            AbstractMinecart parent = ((Chainable)cart).getChainedParent();
-                            parent.setDeltaMovement(Vec3.ZERO);
-                            cart.setDeltaMovement(Vec3.ZERO);
-                        }
+                    while (currentParentCart != null) {
+                        currentParentCart.setDeltaMovement(Vec3.ZERO);
+                        currentCart.setDeltaMovement(Vec3.ZERO);
+
+                        currentCart = currentParentCart;
+                        currentParentCart = ((Chainable) currentCart).getChainedParent();
                     }
-
-                    Chainable.unsetChainedParentChild((Chainable)parentCart, entityIChainable);
-                    entity.spawnAtLocation((ServerLevel) entity.level(), new ItemStack(Items.IRON_CHAIN));
-
-                    NetworkManager.sendRelationshipPayload(entity.getUUID(), null, entity.level());
-                    NetworkManager.sendRelationshipPayload(null, parentCart.getUUID(), entity.level());
-
-                    return;
                 }
 
-                if (entityIChainable.getChainedParent().isRemoved()) {
-                    AbstractMinecart parentCart = entityIChainable.getChainedParent();
+                Chainable.unsetChainedParentChild((Chainable)parentCart, cartChainable);
+                cart.spawnAtLocation((ServerLevel) cart.level(), new ItemStack(Items.IRON_CHAIN));
 
-                    Chainable.unsetChainedParentChild((Chainable)parentCart, entityIChainable);
+                NetworkManager.sendRelationshipPayload(cart.getUUID(), null, cart.level());
+                NetworkManager.sendRelationshipPayload(null, parentCart.getUUID(), cart.level());
 
-                    NetworkManager.sendRelationshipPayload(entity.getUUID(), null, entity.level());
-                    NetworkManager.sendRelationshipPayload(null, parentCart.getUUID(), entity.level());
-                }
+                return;
             }
 
-            if (entityIChainable.getChainedChild() != null && entityIChainable.getChainedChild().isRemoved()) {
-                AbstractMinecart childCart = entityIChainable.getChainedChild();
+            if (parentCart.isRemoved()) {
+                Chainable.unsetChainedParentChild((Chainable)parentCart, cartChainable);
 
-                Chainable.unsetChainedParentChild(entityIChainable, (Chainable)childCart);
-
-                NetworkManager.sendRelationshipPayload(childCart.getUUID(), null, entity.level());
-                NetworkManager.sendRelationshipPayload(null, childCart.getUUID(), entity.level());
+                NetworkManager.sendRelationshipPayload(cart.getUUID(), null, cart.level());
+                NetworkManager.sendRelationshipPayload(null, parentCart.getUUID(), cart.level());
             }
+        }
 
-            for (Entity otherEntity : entity.level().getEntities(entity, entity.getBoundingBox().inflate(0.1))) {
+        if (childCart != null && childCart.isRemoved()) {
+            Chainable.unsetChainedParentChild(cartChainable, (Chainable)childCart);
 
-                if (
-                    otherEntity instanceof AbstractMinecart otherCart
-                    && entityIChainable.getChainedParent() != null
-                    && entityIChainable.getChainedChild() != null
-                    && entityIChainable.getChainedChild() instanceof AbstractMinecart childCart
-                    && !otherCart.equals(childCart)
-                ) {
-                    otherCart.setDeltaMovement(entity.getDeltaMovement());
-                }
-            }
+            NetworkManager.sendRelationshipPayload(childCart.getUUID(), null, cart.level());
+            NetworkManager.sendRelationshipPayload(null, childCart.getUUID(), cart.level());
         }
     }
 }
